b5dd56714e084879e976be6a208dd475102d5596
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2025-03-28 16:06:32 -0700

    disable cmd+shift+p

---
+++ firefox_autoconfig/defaults/pref/autoconfig.js
@@ -0,0 +1,4 @@
+// REQUIRED: Use Unix (LF) line endings for this file
+pref("general.config.filename", "firefox.cfg");
+pref("general.config.obscure_value", 0);
+pref("general.config.sandbox_enabled", false); // <-- THIS LINE IS CRITICAL
